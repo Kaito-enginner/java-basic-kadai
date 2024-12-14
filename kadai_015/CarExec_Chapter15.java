e58bc4cdce54343966bfc8daca2a78c7695ebb5a
Kaito-enginner <itatani.kaito-saber@outlook.jp>
2024-12-14 16:52:09 +0900

    「車クラスを作って速度を出力しよう」kadai_015をアップロード

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,14 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_Chapter15 firstCar = new Car_Chapter15(1 , 10);
+		
+		firstCar.gearChange(3);
+		firstCar.run();
+
+	}
+
+}
